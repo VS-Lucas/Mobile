d14d9ad8eb0b239aa646d5028c7c5651ab8f755e
Severino Carlos <severinocsilva3@gmail.com>
2025-04-09 22:52:52 -0300

    feat: register screen

--- app/src/main/java/com/mobile/barbershop/MainActivity.kt
+++ app/src/main/java/com/mobile/barbershop/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mobile.barbershop
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,35 +17,58 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.mobile.barbershop.databinding.ActivityMainBinding
 import com.mobile.barbershop.ui.theme.BarberShopTheme
 import com.mobile.barbershop.view.Home
+import com.mobile.barbershop.view.Registrar
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btLogin.setOnClickListener {
-            val name = binding.editNome.text.toString()
+        auth = Firebase.auth
+
+        binding?.btLogin?.setOnClickListener {
+            val email = binding.editNome.text.toString()
             val password = binding.editSenha.text.toString()
 
             when {
-                name.isEmpty() -> {
+                email.isEmpty() -> {
+                    Log.d("DEBUG", "Login:: Campo do nome é obrigatório")
                     showSnackbar(it, "O campo nome é obrigatório.")
                 } password.isEmpty() -> {
                     showSnackbar(it, "O campo senha é obrigatório.")
                 } else -> {
-                    // Caso estivesse tudo válido e ele iria para a tela de home
-                    // Aqui vai fazer a lógica do login imagino
-                    goToHome(name)
+                    signIn(email, password)
                 }
             }
+        }
 
+        binding?.createAccount?.setOnClickListener {
+            val intent = Intent(this@MainActivity, Registrar::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("DEBUG", "Login feito com sucesso")
+                val user = auth.currentUser
+                goToHome("Nome do cara")
+            } else {
+                Log.d("DEBUG", "Erro ao fazer login", task.exception)
+                showSnackbar(binding.btLogin, "Email ou senha incorreto!")
+            }
         }
     }
 
---
+++ app/src/main/java/com/mobile/barbershop/view/Registrar.kt
@@ -0,0 +1,74 @@
+package com.mobile.barbershop.view
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.mobile.barbershop.MainActivity
+import com.mobile.barbershop.R
+import com.mobile.barbershop.adapter.ServicosAdapter
+import com.mobile.barbershop.databinding.ActivityHomeBinding
+import com.mobile.barbershop.databinding.ActivityRegistrarBinding
+import com.mobile.barbershop.models.Servicos
+
+class Registrar : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrarBinding
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityRegistrarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding?.registerButton?.setOnClickListener {
+            val email: String = binding?.emailEditText?.text.toString()
+            val password: String = binding?.passwordEditText?.text.toString()
+            val confirmPassword: String = binding?.confirmPasswordEditText?.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
+                if (password == confirmPassword) {
+                    createUser(email, password)
+                } else {
+                    showSnackbar(it, "As senha não são compatíveis")
+                }
+            } else {
+                showSnackbar(it, "Por favor, preencha os campos!")
+            }
+
+        }
+
+    }
+
+    private fun showSnackbar(view: View, message: String) {
+        val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
+        snackbar.setBackgroundTint(Color.parseColor("#FF0000"))
+        snackbar.setTextColor(Color.WHITE)
+        snackbar.show()
+    }
+
+    private fun createUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("DEBUG", "Usuário criado com sucesso")
+                val intent = Intent(this@Registrar, Home::class.java)
+                startActivity(intent)
+            } else {
+                Log.d("DEBUG", "Erro ao criar usuário", task.exception)
+            }
+        }
+    }
+}
